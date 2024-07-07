54cff7170077cfcc742fbb241ce75f13d722a1d3
GHULAM MUSTAFA <161878014+Mustafa-Khan786@users.noreply.github.com>
2024-07-07 11:29:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
+// install date-fns npm for this project from npm
+import { differenceInSeconds } from "date-fns";
 import inquirer from "inquirer";
-const answers = await inquirer.prompt([
-    {
-        type: "number",
-        name: "numone",
-        message: "kindly inter your first number",
-    },
-    { type: "number",
-        name: "numtwo",
-        message: "kindly inter your second number",
-    },
-    {
-        type: "list",
-        name: "opperator",
-        choices: ['addition', 'subtraction', 'multiplication', "division"],
-        message: "Sellect your opperator:"
+const res = await inquirer.prompt({
+    type: "number",
+    name: "userInput",
+    message: "Please enter the time in Seconds :",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "please enter the valid number";
+        }
+        else if (input > 60) {
+            return "Please enter in second like below 60 :";
+        }
+        else {
+            return true;
+        }
     }
-]);
-if (answers.opperator === "addition") {
-    console.log(answers.numone + answers.numtwo);
+});
+let seconds = res.userInput;
+function startTimer(val) {
+    const initialTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(initialTime);
+    setInterval(() => {
+        const currentTime = new Date();
+        const timeDifference = differenceInSeconds(intervalTime, currentTime);
+        if (timeDifference <= 0) {
+            console.log("Timer End");
+            process.exit();
+        }
+        const min = Math.floor((timeDifference % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDifference % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }, 1000);
 }
-if (answers.opperator === "subtraction") {
-    console.log(answers.numone - answers.numtwo);
-}
-if (answers.opperator === "multiplication") {
-    console.log(answers.numone * answers.numtwo);
-}
-if (answers.opperator === "division") {
-    console.log(answers.numone / answers.numtwo);
-}
+startTimer(seconds);
