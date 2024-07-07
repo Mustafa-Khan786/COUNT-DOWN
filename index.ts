63ed130339c161f509aa0eb2b749baa3ac151716
GHULAM MUSTAFA <161878014+Mustafa-Khan786@users.noreply.github.com>
2024-07-07 11:28:37 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,33 +1,40 @@
 
+// install date-fns npm for this project from npm
+import { differenceInSeconds } from  "date-fns";
 import inquirer from "inquirer";
-const answers =await inquirer.prompt([
-{
+
+const res = await inquirer.prompt({
     type:"number",
-    name:"numone",
-    message: "kindly inter your first number",
+    name:"userInput",
+    message:"Please enter the time in Seconds :",
+    validate:(input)=>{
+        if(isNaN(input)){
+            return"please enter the valid number";
+        }else if(input > 60){
+            return "Please enter in second like below 60 :"
+        }else{
+            return true
+        }
 
-}, 
-{type:"number",
-    name:"numtwo",
-     message: "kindly inter your second number",
-
-},
-{
-    type: "list",
-    name:"opperator",
-    choices: ['addition','subtraction','multiplication',"division"],
-    message:"Sellect your opperator:"
-}
-]) 
- if (answers.opperator ==="addition") {
-    console.log(answers.numone + answers.numtwo)   
-}
-if (answers.opperator ==="subtraction") {
-    console.log(answers.numone - answers.numtwo)  
-}
-if (answers.opperator ==="multiplication") {
-    console.log(answers.numone * answers.numtwo)
-}
-if (answers.opperator ==="division") {
-            console.log(answers.numone / answers.numtwo)   
     }
+});
+let seconds = res.userInput
+
+function startTimer(val:number){
+    const initialTime= new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(initialTime);
+    setInterval(()=>{
+        const currentTime = new Date()
+        const timeDifference = differenceInSeconds(intervalTime,currentTime);
+        if(timeDifference <= 0 ){
+            console.log("Timer End");
+            process.exit();
+        }
+        const min= Math.floor((timeDifference % (3600*24))/3600);
+        const sec = Math.floor(timeDifference % 60);
+        console.log(`${min.toString().padStart(2,"0")}:${sec.toString().padStart(2,"0")}`);
+    },1000);
+}
+    
+startTimer(seconds)
+
